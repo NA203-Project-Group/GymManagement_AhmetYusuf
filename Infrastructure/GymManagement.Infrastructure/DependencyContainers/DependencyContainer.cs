63611f58d656f1b3058bdf926e677c0a91dab037
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-03-02 10:01:43 +0300

    .

--- Infrastructure/GymManagement.Infrastructure/DependencyContainers/DependencyContainer.cs
+++ Infrastructure/GymManagement.Infrastructure/DependencyContainers/DependencyContainer.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GymManagement.Application.Interfaces.Repositories;
+using GymManagement.Infrastructure.Contexts;
 using GymManagement.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymManagement.Infrastructure.DependencyContainers
 {
@@ -13,8 +15,17 @@ namespace GymManagement.Infrastructure.DependencyContainers
     {
         public static void AddInfrastructureServices(this IServiceCollection services)
         {
-
+            services.AddDbContext<GymManagementDbContext>(options => options.UseSqlServer());
             services.AddScoped<ICampaignRepository, CampaignRepository>();
+            services.AddScoped<IWorkerRepository, WorkerRepository>();
+            services.AddScoped<IEmployeeDetailRepository, EmployeeDetailRepository>();
+            services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+            services.AddScoped<IExerciseProgramRepository, ExerciseProgramRepository>();
+            services.AddScoped<IManagerRepository, ManagerRepository>();
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddScoped<IMissionRepository, MissionRepository>();
+            services.AddScoped<ITrainerRepository, TrainerRepository>();
+            services.AddScoped<IWorkerContractRepository, WorkerContractRepository>();
 
         }
     }
