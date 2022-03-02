f10200978dd58221815cd546f9f82a7448c4487a
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-03-02 11:59:01 +0300

    Mapping created

---
+++ Core/GymManagement.Application/Mapping/CampaignMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.Application.Mapping
+{
+    public class CampaignMappingProfile:Profile
+    {
+    }
+}
